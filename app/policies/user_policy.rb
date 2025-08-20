f480c0d558ec968e0976004f7964d451f8e106b3
trailsofink <trailsofink@gmail.com>
2025-08-20 17:22:22 -0500

    adding user policy and refactor user auth on profile.

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,16 @@
+# app/policies/user_policy.rb
+
+class UserPolicy < ApplicationPolicy
+  attr_reader :current_user, :user
+
+  def initialize(current_user, user)
+    @current_user = current_user
+    @user = user
+  end
+
+  def show_photos?
+    user == current_user ||
+     !user.private? ||
+     user.followers.include?(current_user)
+  end
+end
